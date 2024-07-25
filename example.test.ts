686268a10edd5533fbca81051fc2c72d9c957294
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2024-07-26 00:24:08 +0200

    npx jest
     PASS  ./example.test.ts
      Calculator
        √ throws error when input.length < 2 (23 ms)
        √ throws error when unsupported operator was used (2 ms)
        √ adds 2 or more numbers incl. `NaN` and `Infinity` (2 ms)
        √ subtracts 2 or more numbers incl. `NaN` and `Infinity` (1 ms)
        √ multiplies 2 or more numbers incl. `NaN` and `Infinity` (2 ms)
        √ divides 2 or more numbers incl. `NaN` and `Infinity` (1 ms)
        √ adds [1, 41] expecting 42
        √ adds [1, 2, 39] expecting 42 (1 ms)
        √ adds [1, 2, NaN] expecting NaN (1 ms)
        √ adds [1, 2, Infinity] expecting Infinity (1 ms)
        √ subtracts [43, 1] expecting 42 (2 ms)
        √ subtracts [44, 1, 1] expecting 42 (2 ms)
        √ subtracts [1, 2, NaN] expecting NaN (1 ms)
        √ subtracts [1, 2, Infinity] expecting -Infinity (3 ms)
        √ multiplies [21, 2] expecting 42
        √ multiplies [3, 7, 2] expecting 42 (1 ms)
        √ multiplies [42, NaN] expecting NaN (1 ms)
        √ multiplies [42, Infinity] expecting Infinity
        √ divides [84, 2] expecting 42 (1 ms)
        √ divides [168, 2, 2] expecting 42
        √ divides [168, 2, 2] expecting 42 (1 ms)
        √ divides [42, 0] expecting Infinity
        √ divides [42, NaN] expecting NaN
    
    Test Suites: 1 passed, 1 total
    Tests:       23 passed, 23 total
    Snapshots:   0 total
    Time:        4.355 s
    Ran all test suites.

--- example.test.ts
+++ example.test.ts
@@ -1,5 +1,6 @@
 import calculator from "./app";
 
+// https://blog.codeleak.pl/2021/12/parameterized-tests-with-jest.html
 describe("Calculator", () => {
   it("throws error when input.length < 2", () => {
     expect(() => calculator("+", [0])).toThrow(
@@ -39,5 +40,42 @@ describe("Calculator", () => {
     expect(calculator("/", [42, 0])).toEqual(Infinity);
     expect(calculator("/", [42, NaN])).toEqual(NaN);
     expect(calculator("/", [168, 2, 2])).toEqual(42);
+  });
+
+  it.each([
+    [[1, 41], 42],
+    [[1, 2, 39], 42],
+    [[1, 2, NaN], NaN],
+    [[1, 2, Infinity], Infinity],
+  ])("adds %p expecting %p", (numbers: number[], result: number) => {
+    expect(calculator("+", numbers)).toEqual(result);
+  });
+
+  it.each([
+    [[43, 1], 42],
+    [[44, 1, 1], 42],
+    [[1, 2, NaN], NaN],
+    [[1, 2, Infinity], -Infinity],
+  ])("subtracts %p expecting %p", (numbers: number[], result: number) => {
+    expect(calculator("-", numbers)).toEqual(result);
+  });
+
+  it.each([
+    [[21, 2], 42],
+    [[3, 7, 2], 42],
+    [[42, NaN], NaN],
+    [[42, Infinity], Infinity],
+  ])("multiplies %p expecting %p", (numbers: number[], result: number) => {
+    expect(calculator("*", numbers)).toEqual(result);
+  });
+
+  it.each([
+    [[84, 2], 42],
+    [[168, 2, 2], 42],
+    [[168, 2, 2], 42],
+    [[42, 0], Infinity],
+    [[42, NaN], NaN],
+  ])("divides %p expecting %p", (numbers: number[], result: number) => {
+    expect(calculator("/", numbers)).toEqual(result);
   });
 });
