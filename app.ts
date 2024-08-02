369de3ee8def83024bfef7442234d44a05a059bd
BowTiedRadone <bowtiedradone@gmail.com>
2024-08-02 19:49:05 +0300

    Add type mappings and `fast-check` utilities for complex Clarity types
    - Introduced new types: `BaseType`, `ComplexType`, and `ArgType` for better type management.
    - Added `ComplexTypesToFcType` mapping for handling complex Fast-Check types like `buffer`, `string-ascii`, `list`, `tuple`, `optional`, and `response`.
    - Implemented `generateArbitrary` function to dynamically generate Fast-Check arbitraries based on type.
    - Added `charSet` constant for ASCII string generation.

--- app.ts
+++ app.ts
@@ -7,6 +7,18 @@ import { Cl, cvToJSON } from "@stacks/transactions";
 import fc from "fast-check";
 import fs from "fs";
 
+export type BaseType = "int128" | "uint128" | "bool" | "principal";
+export type ComplexType =
+  | { buffer: { length: number } }
+  | { "string-ascii": { length: number } }
+  | { "string-utf8": { length: number } }
+  | { list: { type: ArgType; length: number } }
+  | { tuple: { name: string; type: ArgType }[] }
+  | { optional: ArgType }
+  | { response: { ok: ArgType; error: ArgType } };
+
+export type ArgType = BaseType | ComplexType;
+
 /**
  * LocalContext is a data structure used to track the number of times each SUT function is called
  * for every contract. It is a nested map where:
@@ -26,6 +38,31 @@ export type BaseTypesToFcType = {
   bool: ReturnType<typeof fc.boolean>;
   principal: (addresses: string[]) => ReturnType<typeof fc.constantFrom>;
 };
+
+export type ComplexTypesToFcType = {
+  buffer: (length: number) => fc.Arbitrary<string>;
+  "string-ascii": (length: number) => fc.Arbitrary<string>;
+  "string-utf8": (length: number) => fc.Arbitrary<string>;
+  list: (
+    type: ArgType,
+    length: number,
+    addresses: string[]
+  ) => fc.Arbitrary<any[]>;
+  tuple: (
+    items: { name: string; type: ArgType }[],
+    addresses: string[]
+  ) => fc.Arbitrary<object>;
+  optional: (type: ArgType, addresses: string[]) => fc.Arbitrary<any>;
+  response: (
+    okType: ArgType,
+    errType: ArgType,
+    addresses: string[]
+  ) => fc.Arbitrary<any>;
+};
+
+/** The character set used for generating ASCII strings.*/
+const charSet =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 /**
  * The context contract that will be concatenated with the SUT and the invariants contracts.
@@ -47,6 +84,89 @@ export const baseTypesToFC: BaseTypesToFcType = {
   uint128: fc.nat(),
   bool: fc.boolean(),
   principal: (addresses: string[]) => fc.constantFrom(...addresses),
+};
+
+/**
+ * Complex types to fast-check arbitraries mapping.
+ */
+const complexTypesToFC: ComplexTypesToFcType = {
+  buffer: (length: number) => fc.hexaString({ maxLength: length }),
+  "string-ascii": (length: number) =>
+    fc.stringOf(fc.constantFrom(...charSet), {
+      maxLength: length,
+      minLength: 1,
+    }),
+  "string-utf8": (length: number) => fc.string({ maxLength: length }),
+  list: (type: ArgType, length: number, addresses: string[]) =>
+    fc.array(generateArbitrary(type, addresses), { maxLength: length }),
+  tuple: (items: { name: string; type: ArgType }[], addresses: string[]) => {
+    const tupleArbitraries: { [key: string]: fc.Arbitrary<any> } = {};
+    items.forEach((item) => {
+      tupleArbitraries[item.name] = generateArbitrary(item.type, addresses);
+    });
+    return fc.record(tupleArbitraries);
+  },
+  optional: (type: ArgType, addresses: string[]) =>
+    fc.option(generateArbitrary(type, addresses)),
+  response: (okType: ArgType, errType: ArgType, addresses: string[]) =>
+    fc.oneof(
+      fc.record({
+        status: fc.constant("ok"),
+        value: generateArbitrary(okType, addresses),
+      }),
+      fc.record({
+        status: fc.constant("error"),
+        value: generateArbitrary(errType, addresses),
+      })
+    ),
+};
+
+/**
+ * For a given type, generate a fast-check arbitrary.
+ * @param type
+ * @returns fast-check arbitrary
+ */
+const generateArbitrary = (
+  type: ArgType,
+  addresses: string[]
+): fc.Arbitrary<any> => {
+  if (typeof type === "string") {
+    // The type is a base type
+    if (type === "principal") {
+      if (addresses.length === 0)
+        throw new Error(
+          "No addresses could be retrieved from the simnet instance!"
+        );
+      return baseTypesToFC.principal(addresses);
+    } else return baseTypesToFC[type];
+  } else {
+    // The type is a complex type
+    if ("buffer" in type) {
+      return complexTypesToFC["buffer"](type.buffer.length);
+    } else if ("string-ascii" in type) {
+      return complexTypesToFC["string-ascii"](type["string-ascii"].length);
+    } else if ("string-utf8" in type) {
+      return complexTypesToFC["string-utf8"](type["string-utf8"].length);
+    } else if ("list" in type) {
+      return complexTypesToFC["list"](
+        type.list.type,
+        type.list.length,
+        addresses
+      );
+    } else if ("tuple" in type) {
+      return complexTypesToFC["tuple"](type.tuple, addresses);
+    } else if ("optional" in type) {
+      return complexTypesToFC["optional"](type.optional, addresses);
+    } else if ("response" in type) {
+      return complexTypesToFC.response(
+        type.response.ok,
+        type.response.error,
+        addresses
+      );
+    } else {
+      throw new Error(`Unsupported complex type: ${JSON.stringify(type)}`);
+    }
+  }
 };
 
 /**
