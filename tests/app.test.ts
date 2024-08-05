0139cb6ac3c1a14001af74aea48371aa428cc0ee
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2024-08-05 15:16:35 +0200

    Add contexatenate function to merge contracts with context

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,10 +1,34 @@
-import { main } from "../app";
+import { contexatenate, main } from "../app";
+import fc from "fast-check";
 
 // https://blog.codeleak.pl/2021/12/parameterized-tests-with-jest.html
 describe("Manifest handling", () => {
   it("throws error when manifest path is not provided", () => {
     expect(async () => await main()).rejects.toThrow(
       "No path to Clarinet.toml manifest provided. Supply it immediately or face the relentless scrutiny of your contract's vulnerabilities."
+    );
+  });
+});
+
+describe("Contract concatenation", () => {
+  const context = `(define-map context (string-ascii 100) {
+    called: uint
+    ;; other data
+  })
+
+  (define-public (update-context (function-name (string-ascii 100)) (called uint))
+    (ok (map-set context function-name {called: called})))`;
+
+  it("adds context between contract and invariants", () => {
+    fc.assert(
+      // Arrange
+      fc.property(fc.string(), fc.string(), (contract, invariants) => {
+        // Act
+        const actual = contexatenate(contract, invariants);
+        // Assert
+        const expected = `${contract}\n\n${context}\n\n${invariants}`;
+        expect(actual).toBe(expected);
+      }),
     );
   });
 });
