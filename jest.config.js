8d4fce2603c163aa4b3899b98613db738aa8dc27
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2024-08-06 08:11:21 +0200

    Update Jest config to run only TypeScript test files
    - Added testMatch to jest.config.js to run .test.ts files only.
    - Updated test script in package.json to use Jest directly.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
