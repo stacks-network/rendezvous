986f9c4e963e0cbc98998be01356c1fe8e2cb81c
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2024-08-09 13:03:37 +0200

    Update entry point's error message

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -23,7 +23,7 @@ import { Cl } from "@stacks/transactions";
 describe("Manifest handling", () => {
   it("throws error when manifest path is not provided", () => {
     expect(async () => await main()).rejects.toThrow(
-      "No path to Clarinet.toml manifest provided. Supply it immediately or face the relentless scrutiny of your contract's vulnerabilities."
+      "No path to Clarinet project provided. Supply it immediately or face the relentless scrutiny of your contract's vulnerabilities."
     );
   });
 });
