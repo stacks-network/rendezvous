e263b2aeeed76cb086812c03921e09060b1e2c93
BowTiedRadone <bowtiedradone@gmail.com>
2024-12-06 00:07:08 +0200

    Add `citizen` tests and update existing ones
    This commit adds tests for new `citizen` features, updates existing ones, and
    removes deprecated tests. Functions from `citizen` are now exported for testing.

--- invariant.tests.ts
+++ invariant.tests.ts
@@ -1,15 +1,22 @@
 import { initSimnet } from "@hirosystems/clarinet-sdk";
-import { initializeLocalContext } from "./invariant";
+import {
+  getContractNameFromRendezvousId,
+  initializeClarityContext,
+  initializeLocalContext,
+} from "./invariant";
 import {
   getFunctionsFromContractInterfaces,
   getSimnetDeployerContractsInterfaces,
 } from "./shared";
-import { resolve } from "path";
+import { join } from "path";
+import { issueFirstClassCitizenship } from "./citizen";
+import { Cl } from "@stacks/transactions";
+import fc from "fast-check";
 
 describe("Simnet contracts operations", () => {
   it("correctly initializes the local context for a given functions map", async () => {
     // Arrange
-    const manifestPath = resolve(__dirname, "./example/Clarinet.toml");
+    const manifestPath = join("example", "Clarinet.toml");
     const simnet = await initSimnet(manifestPath);
     const sutContractsInterfaces = getSimnetDeployerContractsInterfaces(simnet);
     const sutContractsAllFunctions = getFunctionsFromContractInterfaces(
@@ -34,63 +41,83 @@ describe("Simnet contracts operations", () => {
     expect(actualInitialContext).toEqual(expectedInitialContext);
   });
 
-  // FIXME: Update this test to use the new `original contract` approach.
-  // it("correctly initializes the Clarity context", async () => {
-  //   // Arrange
-  //   const manifestPath = resolve(__dirname, "./example/Clarinet.toml");
-  //   const contractsPath = resolve(__dirname, "./example/contracts");
-  //   const simnet = await initSimnet(manifestPath);
-  //   const sutContractsInterfaces = getSimnetDeployerContractsInterfaces(simnet);
-  //   const sutContractsList = Array.from(sutContractsInterfaces.keys());
-  //   sutContractsList
-  //     .map((contractId) =>
-  //       buildRendezvousData(simnet, contractId, contractsPath)
-  //     )
-  //     .forEach((contractData) => {
-  //       deployRendezvous(
-  //         simnet,
-  //         contractData.rendezvousFileName,
-  //         contractData.rendezvousSource
-  //       );
-  //     });
-  //   const rendezvousAllFunctions = getFunctionsFromContractInterfaces(
-  //     new Map(Array.from(getSimnetDeployerContractsInterfaces(simnet)))
-  //   );
+  it("correctly initializes the Clarity context", async () => {
+    // Arrange
+    const simnet = await issueFirstClassCitizenship("example", "counter");
 
-  //   // The JS representation of Clarity `(some (tuple (called uint)))`, where `called` is
-  //   // initialized to 0.
-  //   const expectedClarityValue = Cl.some(Cl.tuple({ called: Cl.uint(0) }));
-  //   const expectedContext = Array.from(rendezvousAllFunctions).flatMap(
-  //     ([contractId, functions]) =>
-  //       functions.map((f) => {
-  //         return {
-  //           contractId,
-  //           functionName: f.name,
-  //           called: expectedClarityValue,
-  //         };
-  //       })
-  //   );
+    const rendezvousList = Array.from(
+      getSimnetDeployerContractsInterfaces(simnet).keys()
+    ).filter((deployedContract) =>
+      ["counter"].includes(deployedContract.split(".")[1])
+    );
 
-  //   // Act
-  //   initializeClarityContext(simnet, rendezvousAllFunctions);
+    const rendezvousAllFunctions = getFunctionsFromContractInterfaces(
+      new Map(
+        Array.from(getSimnetDeployerContractsInterfaces(simnet)).filter(
+          ([contractId]) => rendezvousList.includes(contractId)
+        )
+      )
+    );
 
-  //   const actualContext = Array.from(rendezvousAllFunctions).flatMap(
-  //     ([contractId, functions]) =>
-  //       functions.map((f) => {
-  //         const actualValue = simnet.getMapEntry(
-  //           contractId,
-  //           "context",
-  //           Cl.stringAscii(f.name)
-  //         );
-  //         return {
-  //           contractId,
-  //           functionName: f.name,
-  //           called: actualValue,
-  //         };
-  //       })
-  //   );
+    // Act
+    initializeClarityContext(simnet, rendezvousAllFunctions);
 
-  //   // Assert
-  //   expect(actualContext).toEqual(expectedContext);
-  // });
+    const actualContext = Array.from(rendezvousAllFunctions).flatMap(
+      ([contractId, functions]) =>
+        functions.map((f) => {
+          const actualValue = simnet.getMapEntry(
+            contractId,
+            "context",
+            Cl.stringAscii(f.name)
+          );
+          return {
+            contractId,
+            functionName: f.name,
+            called: actualValue,
+          };
+        })
+    );
+
+    // Assert
+
+    // The JS representation of Clarity `(some (tuple (called uint)))`, where
+    // `called` is initialized to 0.
+    const expectedClarityValue = Cl.some(Cl.tuple({ called: Cl.uint(0) }));
+    const expectedContext = Array.from(rendezvousAllFunctions).flatMap(
+      ([contractId, functions]) =>
+        functions.map((f) => {
+          return {
+            contractId,
+            functionName: f.name,
+            called: expectedClarityValue,
+          };
+        })
+    );
+
+    expect(actualContext).toEqual(expectedContext);
+  });
+});
+
+describe("Rendezvous contract name", () => {
+  it("gets contract name from Rendezvous contract name", () => {
+    const addressCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const contractNameCharset =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    fc.assert(
+      // Arrange
+      fc.property(
+        fc.stringOf(fc.constantFrom(...addressCharset)),
+        fc.stringOf(fc.constantFrom(...contractNameCharset)),
+        (address, contractName) => {
+          const rendezvousId = `${address}.${contractName}_rendezvous`;
+
+          // Act
+          const actual = getContractNameFromRendezvousId(rendezvousId);
+
+          // Assert
+          expect(actual).toBe(contractName);
+        }
+      )
+    );
+  });
 });
--- shared.tests.ts
+++ shared.tests.ts
@@ -5,9 +5,6 @@ import {
   getSimnetDeployerContractsInterfaces,
 } from "./shared";
 import { resolve } from "path";
-import fc from "fast-check";
-import { getContractNameFromRendezvousId } from "./invariant";
-import { scheduleRendezvous } from "./citizen";
 
 describe("Simnet contracts operations", () => {
   it("retrieves the contracts from the simnet", async () => {
@@ -72,108 +69,5 @@ describe("Simnet contracts operations", () => {
 
     // Assert
     expect(actualAllFunctionsMap).toEqual(expectedAllFunctionsMap);
-  });
-
-  // it("retrieves Rendezvous contracts data", async () => {
-  //   // Arrange
-  //   const manifestPath = resolve(__dirname, "./example/Clarinet.toml");
-  //   const contractsPath = resolve(__dirname, "./example/contracts");
-  //   const simnet = await initSimnet(manifestPath);
-  //   const sutContractsInterfaces = getSimnetDeployerContractsInterfaces(simnet);
-  //   const sutContractsList = Array.from(sutContractsInterfaces.keys());
-
-  //   const expectedRendezvousData = sutContractsList.map((contractId) => {
-  //     const sutContractSource = getSimnetContractSource(simnet, contractId);
-  //     const invariantContractSource = getTestContractSource(
-  //       contractsPath,
-  //       contractId
-  //     );
-  //     const rendezvousSource = scheduleRendezvous(
-  //       sutContractSource!,
-  //       invariantContractSource
-  //     );
-
-  //     return {
-  //       rendezvousFileName: deriveRendezvousName(contractId),
-  //       rendezvousSource,
-  //       rendezvousContractId: contractId,
-  //       rendezvousContractName: contractId.split(".")[1],
-  //     };
-  //   });
-
-  //   // Act
-  //   const actualRendezvousData = sutContractsList.map((contractId) =>
-  //     buildRendezvousData(simnet, contractId, contractsPath)
-  //   );
-
-  //   // Assert
-  //   expect(actualRendezvousData).toEqual(expectedRendezvousData);
-  // });
-});
-
-describe("File stream operations", () => {
-  // it("retrieves the test contract source", async () => {
-  //   // Arrange
-  //   const manifestPath = resolve(__dirname, "./example/Clarinet.toml");
-  //   const contractsPath = resolve(__dirname, "./example/contracts");
-  //   const simnet = await initSimnet(manifestPath);
-  //   const sutContractsInterfaces = getSimnetDeployerContractsInterfaces(simnet);
-  //   const sutContractsList = Array.from(sutContractsInterfaces.keys());
-  //   const expectedTestContractSources = sutContractsList.map((contractId) => {
-  //     const testContractName = `${contractId.split(".")[1]}.tests`;
-  //     const testContractPath = `${contractsPath}/${testContractName}.clar`;
-  //     return fs.readFileSync(testContractPath).toString();
-  //   });
-  //   // Act
-  //   const actualTestContractSources = sutContractsList.map((contractId) =>
-  //     getTestContractSource(contractsPath, contractId)
-  //   );
-  //   // Assert
-  //   expect(actualTestContractSources).toEqual(expectedTestContractSources);
-  // });
-});
-
-describe("Successfully schedules rendezvous", () => {
-  const context = `(define-map context (string-ascii 100) {
-    called: uint
-    ;; other data
-  })
-
-  (define-public (update-context (function-name (string-ascii 100)) (called uint))
-    (ok (map-set context function-name {called: called})))`;
-
-  it("adds context between contract and invariants", () => {
-    fc.assert(
-      // Arrange
-      fc.property(fc.string(), fc.string(), (contract, invariants) => {
-        // Act
-        const actual = scheduleRendezvous(contract, invariants);
-        // Assert
-        const expected = `${contract}\n\n${context}\n\n${invariants}`;
-        expect(actual).toBe(expected);
-      })
-    );
-  });
-
-  it("gets contract name from Rendezvous contract name", () => {
-    const addressCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    const contractNameCharset =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-    fc.assert(
-      // Arrange
-      fc.property(
-        fc.stringOf(fc.constantFrom(...addressCharset)),
-        fc.stringOf(fc.constantFrom(...contractNameCharset)),
-        (address, contractName) => {
-          const rendezvousId = `${address}.${contractName}_rendezvous`;
-
-          // Act
-          const actual = getContractNameFromRendezvousId(rendezvousId);
-
-          // Assert
-          expect(actual).toBe(contractName);
-        }
-      )
-    );
   });
 });
