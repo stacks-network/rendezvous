5188c16ceb860e073f5d051e57292608808d050c
BowTiedRadone <bowtiedradone@gmail.com>
2024-12-19 16:09:48 +0200

    Double hex length for Clarity `buff` type generation
    This commit ensures that the length of generated hex values for Clarity `buff`
    types is doubled, as they are represented in hexadecimal format. The `argToCV`
    function requires this adjustment to process buffer types correctly. Reference:
    https://github.com/hirosystems/stacks.js/blob/fd0bf26b5f29fc3c1bf79581d0ad9b89f0d7f15a/packages/common/src/utils.ts#L522.

--- shared.ts
+++ shared.ts
@@ -142,7 +142,12 @@ const baseTypesToArbitrary: BaseTypesToArbitrary = {
  * Complex types to fast-check arbitraries mapping.
  */
 const complexTypesToArbitrary: ComplexTypesToArbitrary = {
-  buffer: (length: number) => fc.hexaString({ maxLength: length }),
+  // For buffer types, the length must be doubled because they are represented
+  // in hex. The `argToCV` function expects this format for `buff` ClarityValue
+  // conversion. The UInt8Array will have half the length of the corresponding
+  // hex string. Stacks.js reference:
+  // https://github.com/hirosystems/stacks.js/blob/fd0bf26b5f29fc3c1bf79581d0ad9b89f0d7f15a/packages/common/src/utils.ts#L522
+  buffer: (length: number) => fc.hexaString({ maxLength: 2 * length }),
   "string-ascii": (length: number) =>
     fc.string({
       unit: fc.constantFrom(...charSet),
