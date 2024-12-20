00b9facea79a577f173369736510a9acddd57b22
BowTiedRadone <bowtiedradone@gmail.com>
2024-12-20 19:22:16 +0200

    Use randomly generated `numSamples`
    This commit addresses https://github.com/stacks-network/rendezvous/pull/69#discussion_r1894060039.

--- sample.tests.ts
+++ sample.tests.ts
@@ -8,57 +8,67 @@ describe("Fast-check deprecated generators replacement validation", () => {
 
   it("string-ascii Clarity type corresponding generator", () => {
     fc.assert(
-      fc.property(fc.nat(), (length) => {
-        const deprecatedGenerator = fc.stringOf(fc.constantFrom(...charSet), {
-          maxLength: length,
-          minLength: 0,
-        });
-        const replacementGenerator = fc.string({
-          unit: fc.constantFrom(...charSet),
-          maxLength: length,
-        });
+      fc.property(
+        fc.nat(),
+        fc.integer({ min: 7, max: 20 }),
+        (length, numSamples) => {
+          const deprecatedGenerator = fc.stringOf(fc.constantFrom(...charSet), {
+            maxLength: length,
+            minLength: 0,
+          });
+          const replacementGenerator = fc.string({
+            unit: fc.constantFrom(...charSet),
+            maxLength: length,
+          });
 
-        const comparisonResults = compareGenerators(
-          deprecatedGenerator,
-          replacementGenerator
-        );
-        const isMatching = comparisonResults.every(
-          (result) => result.match.matches
-        );
-        const mismatchedItems = comparisonResults.flatMap(
-          (result) => result.match.mismatchedItems
-        );
+          const comparisonResults = compareGenerators(
+            deprecatedGenerator,
+            replacementGenerator,
+            numSamples
+          );
+          const isMatching = comparisonResults.every(
+            (result) => result.match.matches
+          );
+          const mismatchedItems = comparisonResults.flatMap(
+            (result) => result.match.mismatchedItems
+          );
 
-        expect(mismatchedItems).toEqual([]);
-        expect(isMatching).toBe(true);
-      })
+          expect(mismatchedItems).toEqual([]);
+          expect(isMatching).toBe(true);
+        }
+      )
     );
   });
 
   it("buff Clarity type corresponding generator", () => {
     fc.assert(
-      fc.property(fc.nat(), (length) => {
-        const deprecatedGenerator = fc.hexaString({ maxLength: 2 * length });
+      fc.property(
+        fc.nat(),
+        fc.integer({ min: 7, max: 20 }),
+        (length, numSamples) => {
+          const deprecatedGenerator = fc.hexaString({ maxLength: 2 * length });
 
-        const replacementGenerator = hexaString({
-          maxLength: 2 * length,
-        });
+          const replacementGenerator = hexaString({
+            maxLength: 2 * length,
+          });
 
-        const comparisonResults = compareGenerators(
-          deprecatedGenerator,
-          replacementGenerator
-        );
+          const comparisonResults = compareGenerators(
+            deprecatedGenerator,
+            replacementGenerator,
+            numSamples
+          );
 
-        const isMatching = comparisonResults.every(
-          (result) => result.match.matches
-        );
-        const mismatchedItems = comparisonResults.flatMap(
-          (result) => result.match.mismatchedItems
-        );
+          const isMatching = comparisonResults.every(
+            (result) => result.match.matches
+          );
+          const mismatchedItems = comparisonResults.flatMap(
+            (result) => result.match.mismatchedItems
+          );
 
-        expect(mismatchedItems).toEqual([]);
-        expect(isMatching).toBe(true);
-      })
+          expect(mismatchedItems).toEqual([]);
+          expect(isMatching).toBe(true);
+        }
+      )
     );
   });
 });
