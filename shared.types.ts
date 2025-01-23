170b33e5c366d68763a31172af1b69494e5a3152
BowTiedRadone <bowtiedradone@gmail.com>
2025-01-23 19:55:36 +0200

    Housekeeping for random trait reference selection functionality
    Squashes commits:
    - 4b00796422afb0ee31e76a40bc9ac98105d03c8a
    - 89f0bcc0666bfc32a8a89f74f0d5ae1599f8d1f6
    - a1fe34cb05ef9434a29456aa7a62fa1900f328d8
    - 24a076265ea771ba3afa204390982299ef85e7e7
    - 5cf6a5c3ddc035faf5515c3043962930c9481e3f
    - 1e1a29361499b74743a80cfbb6634c11a97425f4
    - 5761171b5e28eb0d9433bc4acee410af09692b15
    - 08684514423154ec486a077b8b7e9a5dfa0a64a4

--- shared.types.ts
+++ shared.types.ts
@@ -23,16 +23,19 @@ import { ImplementedTraitType, ImportedTraitType } from "./traits.types";
 
 // Types used for Clarity Value conversion.
 
+type ImportedTraitReferenceFunctionArg = {
+  type: {
+    trait_reference: ImportedTraitType;
+  };
+  name: string;
+};
+
+/**
+ * The type of the function interface, after the contract interface is
+ * "enriched" with additional information about trait references.
+ */
 export type EnrichedContractInterfaceFunction = {
-  args: (
-    | ContractInterfaceFunctionArg
-    | {
-        type: {
-          trait_reference: ImportedTraitType;
-        };
-        name: string;
-      }
-  )[];
+  args: (ContractInterfaceFunctionArg | ImportedTraitReferenceFunctionArg)[];
   name: string;
   access: ContractInterfaceFunctionAccess;
   outputs: ContractInterfaceFunctionOutput;
@@ -66,16 +69,38 @@ export type ComplexTypesToCV = {
 };
 
 // Types used for argument generation.
-export type BaseTypeBeforeEnrich =
-  | "int"
-  | "uint"
+
+/** The base Clarity parameter types, as found in the contract interface. */
+export type BaseType =
+  | "int128"
+  | "uint128"
   | "bool"
   | "principal"
   | "trait_reference";
 
-export type BaseTypeAfterEnrich = "int128" | "uint128" | "bool" | "principal";
+/**
+ * The base Clarity parameter types, after the contract interface is "enriched"
+ * with additional information about trait references. The "trait_reference" is
+ * no longer a base type after "enrichment".
+ */
+export type EnrichedBaseType = "int128" | "uint128" | "bool" | "principal";
 
-type ComplexTypeAfterEnrich =
+/** The complex Clarity parameter types, as found in the contract interface. */
+type ComplexType =
+  | { buffer: { length: number } }
+  | { "string-ascii": { length: number } }
+  | { "string-utf8": { length: number } }
+  | { list: { type: ParameterType; length: number } }
+  | { tuple: { name: string; type: ParameterType }[] }
+  | { optional: ParameterType }
+  | { response: { ok: ParameterType; error: ParameterType } };
+
+/**
+ * The complex Clarity parameter types, after the contract interface is
+ * "enriched" with additional information about trait references. The
+ * "trait_reference" is a complex type after enrichment.
+ */
+type EnrichedComplexType =
   | { buffer: { length: number } }
   | { "string-ascii": { length: number } }
   | { "string-utf8": { length: number } }
@@ -85,30 +110,17 @@ type ComplexTypeAfterEnrich =
   | { response: { ok: EnrichedParameterType; error: EnrichedParameterType } }
   | { trait_reference: ImportedTraitType };
 
-type ComplexTypeBeforeEnrich =
-  | { buffer: { length: number } }
-  | { "string-ascii": { length: number } }
-  | { "string-utf8": { length: number } }
-  | { list: { type: EnrichedParameterType; length: number } }
-  | { tuple: { name: string; type: EnrichedParameterType }[] }
-  | { optional: EnrichedParameterType }
-  | { response: { ok: EnrichedParameterType; error: EnrichedParameterType } };
+/** The Clarity parameter types as found in the contract interface. */
+export type ParameterType = BaseType | ComplexType;
 
-export type EnrichedParameterType =
-  | BaseTypeAfterEnrich
-  | ComplexTypeAfterEnrich;
-
-export type ParameterTypeBeforeEnrich =
-  | BaseTypeBeforeEnrich
-  | ComplexTypeBeforeEnrich;
+/** The Clarity parameter types after the contract interface is "enriched". */
+export type EnrichedParameterType = EnrichedBaseType | EnrichedComplexType;
 
 export type BaseTypesToArbitrary = {
   int128: ReturnType<typeof fc.integer>;
   uint128: ReturnType<typeof fc.nat>;
   bool: ReturnType<typeof fc.boolean>;
   principal: (addresses: string[]) => ReturnType<typeof fc.constantFrom>;
-  // Trait reference is not yet supported. This is a placeholder.
-  trait_reference: undefined;
 };
 
 export type ComplexTypesToArbitrary = {
