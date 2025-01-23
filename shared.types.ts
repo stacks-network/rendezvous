stacks-network/rendezvous

commit ae0d20c9f8fb26b06362df51bfcd614ce1afd94c

WIP: Add support for direct and tuple-nested traits in property testing

shared.types.ts
@@ -1,3 +1,9 @@
+import { Simnet } from "@hirosystems/clarinet-sdk";
+import {
+  ContractInterfaceFunctionAccess,
+  ContractInterfaceFunctionArg,
+  ContractInterfaceFunctionOutput,
+} from "@hirosystems/clarinet-sdk-wasm";
 import {
   boolCV,
   bufferCV,
@@ -16,6 +22,21 @@ import {
 import fc from "fast-check";
 
 // Types used for Clarity Value conversion.
+
+export type EnrichedContractInterfaceFunction = {
+  args: (
+    | ContractInterfaceFunctionArg
+    | {
+        type: {
+          trait_reference: TraitImportType;
+        };
+        name: string;
+      }
+  )[];
+  name: string;
+  access: ContractInterfaceFunctionAccess;
+  outputs: ContractInterfaceFunctionOutput;
+};
 
 export type ResponseStatus = "ok" | "error";
 
@@ -41,27 +62,45 @@ export type ComplexTypesToCV = {
     status: ResponseStatus,
     value: ClarityValue
   ) => ReturnType<typeof responseOkCV | typeof responseErrorCV>;
+  trait_reference: (trait: string) => ReturnType<typeof principalCV>;
 };
 
 // Types used for argument generation.
-
-export type BaseType =
-  | "int128"
-  | "uint128"
+export type BaseTypeBeforeEnrich =
+  | "int"
+  | "uint"
   | "bool"
   | "principal"
   | "trait_reference";
 
-type ComplexType =
+export type BaseTypeAfterEnrich = "int128" | "uint128" | "bool" | "principal";
+
+type ComplexTypeAfterEnrich =
   | { buffer: { length: number } }
   | { "string-ascii": { length: number } }
   | { "string-utf8": { length: number } }
-  | { list: { type: ParameterType; length: number } }
-  | { tuple: { name: string; type: ParameterType }[] }
-  | { optional: ParameterType }
-  | { response: { ok: ParameterType; error: ParameterType } };
+  | { list: { type: EnrichedParameterType; length: number } }
+  | { tuple: { name: string; type: EnrichedParameterType }[] }
+  | { optional: EnrichedParameterType }
+  | { response: { ok: EnrichedParameterType; error: EnrichedParameterType } }
+  | { trait_reference: TraitImportType };
 
-export type ParameterType = BaseType | ComplexType;
+type ComplexTypeBeforeEnrich =
+  | { buffer: { length: number } }
+  | { "string-ascii": { length: number } }
+  | { "string-utf8": { length: number } }
+  | { list: { type: EnrichedParameterType; length: number } }
+  | { tuple: { name: string; type: EnrichedParameterType }[] }
+  | { optional: EnrichedParameterType }
+  | { response: { ok: EnrichedParameterType; error: EnrichedParameterType } };
+
+export type EnrichedParameterType =
+  | BaseTypeAfterEnrich
+  | ComplexTypeAfterEnrich;
+
+export type ParameterTypeBeforeEnrich =
+  | BaseTypeBeforeEnrich
+  | ComplexTypeBeforeEnrich;
 
 export type BaseTypesToArbitrary = {
   int128: ReturnType<typeof fc.integer>;
@@ -77,18 +116,39 @@ export type ComplexTypesToArbitrary = {
   "string-ascii": (length: number) => fc.Arbitrary<string>;
   "string-utf8": (length: number) => fc.Arbitrary<string>;
   list: (
-    type: ParameterType,
+    type: EnrichedParameterType,
     length: number,
-    addresses: string[]
+    addresses: string[],
+    simnet: Simnet
   ) => fc.Arbitrary<any[]>;
   tuple: (
-    items: { name: string; type: ParameterType }[],
-    addresses: string[]
+    items: { name: string; type: EnrichedParameterType }[],
+    addresses: string[],
+    simnet: Simnet
   ) => fc.Arbitrary<object>;
-  optional: (type: ParameterType, addresses: string[]) => fc.Arbitrary<any>;
-  response: (
-    okType: ParameterType,
-    errType: ParameterType,
-    addresses: string[]
+  optional: (
+    type: EnrichedParameterType,
+    addresses: string[],
+    simnet: Simnet
   ) => fc.Arbitrary<any>;
+  response: (
+    okType: EnrichedParameterType,
+    errType: EnrichedParameterType,
+    addresses: string[],
+    simnet: Simnet
+  ) => fc.Arbitrary<any>;
+  trait_reference: (
+    traitData: TraitImportType,
+    simnet: Simnet
+  ) => fc.Arbitrary<any>;
+};
+
+export type TraitImportType = {
+  name: string;
+  import: {
+    Imported: {
+      name: string;
+      contract_identifier: { issuer: Array<any>; name: string };
+    };
+  };
 };

citizen.tests.ts
@@ -81,11 +81,23 @@ describe("Simnet deployment plan operations", () => {
 
     // Verify
     const expected = {
-      "2.1": [
+      "2.4": [
+        {
+          dao: {
+            clarity_version: 2,
+            path: "./.cache/requirements/SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.dao.clar",
+          },
+        },
         {
           "sip-010-trait-ft-standard": {
-            clarity_version: 1,
-            path: "./.cache/requirements/SP3FBR2AGK5H9QBDH3EEN6DF8EK8JY7RX8QJ5SVTE.sip-010-trait-ft-standard.clar",
+            clarity_version: 2,
+            path: "./.cache/requirements/SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.sip-010-trait-ft-standard.clar",
+          },
+        },
+        {
+          "ststx-token": {
+            clarity_version: 2,
+            path: "./.cache/requirements/SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.ststx-token.clar",
           },
         },
       ],

example/.cache/requirements/SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.dao.clar
@@ -1,0 +1,144 @@
+;; @contract DAO
+;; @version 1
+
+;;-------------------------------------
+;; Constants 
+;;-------------------------------------
+
+(define-constant ERR_NOT_ADMIN u20001)
+(define-constant ERR_CONTRACTS_DISABLED u20002)
+(define-constant ERR_INACTIVE_CONTRACT u20003)
+
+;;-------------------------------------
+;; Variables 
+;;-------------------------------------
+
+(define-data-var contracts-enabled bool true)
+
+;;-------------------------------------
+;; Maps 
+;;-------------------------------------
+
+(define-map contracts
+  { 
+    address: principal 
+  }
+  {
+    active: bool,
+  }
+)
+
+(define-map admins
+  { 
+    address: principal 
+  }
+  {
+    active: bool,
+  }
+)
+
+;;-------------------------------------
+;; Getters 
+;;-------------------------------------
+
+(define-read-only (get-contracts-enabled)
+  (var-get contracts-enabled)
+)
+
+(define-read-only (get-contract-active (address principal))
+  (get active 
+    (default-to 
+      { active: false }
+      (map-get? contracts { address: address })
+    )
+  )
+)
+
+(define-read-only (get-admin (address principal))
+  (get active 
+    (default-to 
+      { active: false }
+      (map-get? admins { address: address })
+    )
+  )
+)
+
+;;-------------------------------------
+;; Checks 
+;;-------------------------------------
+
+(define-public (check-is-enabled)
+  (begin
+    (asserts! (var-get contracts-enabled) (err ERR_CONTRACTS_DISABLED))
+    (ok true)
+  )
+)
+
+(define-public (check-is-protocol (contract principal))
+  (begin
+    (asserts! (get-contract-active contract) (err ERR_INACTIVE_CONTRACT))
+    (ok true)
+  )
+)
+
+(define-public (check-is-admin (contract principal))
+  (begin
+    (asserts! (get-admin contract) (err ERR_NOT_ADMIN))
+    (ok true)
+  )
+)
+
+;;-------------------------------------
+;; Set 
+;;-------------------------------------
+
+(define-public (set-contracts-enabled (enabled bool))
+  (begin
+    (try! (check-is-admin tx-sender))
+    (var-set contracts-enabled enabled)
+    (ok true)
+  )
+)
+
+(define-public (set-contract-active (address principal) (active bool))
+  (begin
+    (try! (check-is-admin tx-sender))
+    (map-set contracts { address: address } { active: active }
+  )
+    (ok true)
+  )
+)
+
+(define-public (set-admin (address principal) (active bool))
+  (begin
+    (try! (check-is-admin tx-sender))
+    (map-set admins { address: address } { active: active }
+  )
+    (ok true)
+  )
+)
+
+;;-------------------------------------
+;; Init 
+;;-------------------------------------
+
+(begin
+  (map-set admins { address: tx-sender } { active: true })
+
+  (map-set contracts { address: tx-sender } { active: true })
+
+  (map-set contracts { address: .stacking-dao-core-v1 } { active: true })
+  (map-set contracts { address: .reserve-v1 } { active: true })
+  (map-set contracts { address: .commission-v1 } { active: true })
+
+  (map-set contracts { address: .stacker-1 } { active: true })
+  (map-set contracts { address: .stacker-2 } { active: true })
+  (map-set contracts { address: .stacker-3 } { active: true })
+  (map-set contracts { address: .stacker-4 } { active: true })
+  (map-set contracts { address: .stacker-5 } { active: true })
+  (map-set contracts { address: .stacker-6 } { active: true })
+  (map-set contracts { address: .stacker-7 } { active: true })
+  (map-set contracts { address: .stacker-8 } { active: true })
+  (map-set contracts { address: .stacker-9 } { active: true })
+  (map-set contracts { address: .stacker-10 } { active: true })
+)

example/.cache/requirements/SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.ststx-token.clar
@@ -1,0 +1,93 @@
+(impl-trait .sip-010-trait-ft-standard.sip-010-trait)
+
+;; Defines the Stacked STX according to the SIP010 Standard
+(define-fungible-token ststx)
+
+(define-constant ERR_NOT_AUTHORIZED u1401)
+
+;;-------------------------------------
+;; Variables
+;;-------------------------------------
+
+(define-data-var token-uri (string-utf8 256) u"")
+
+;;-------------------------------------
+;; SIP-010 
+;;-------------------------------------
+
+(define-read-only (get-total-supply)
+  (ok (ft-get-supply ststx))
+)
+
+(define-read-only (get-name)
+  (ok "Stacked STX Token")
+)
+
+(define-read-only (get-symbol)
+  (ok "stSTX")
+)
+
+(define-read-only (get-decimals)
+  (ok u6)
+)
+
+(define-read-only (get-balance (account principal))
+  (ok (ft-get-balance ststx account))
+)
+
+(define-read-only (get-token-uri)
+  (ok (some (var-get token-uri)))
+)
+
+(define-public (transfer (amount uint) (sender principal) (recipient principal) (memo (optional (buff 34))))
+  (begin
+    (asserts! (is-eq tx-sender sender) (err ERR_NOT_AUTHORIZED))
+
+    (match (ft-transfer? ststx amount sender recipient)
+      response (begin
+        (print memo)
+        (print { action: "transfer", data: { sender: tx-sender, recipient: recipient, amount: amount, block-height: block-height } })
+        (ok response)
+      )
+      error (err error)
+    )
+  )
+)
+
+;;-------------------------------------
+;; Admin
+;;-------------------------------------
+
+(define-public (set-token-uri (value (string-utf8 256)))
+  (begin
+    (try! (contract-call? .dao check-is-protocol tx-sender))
+    (ok (var-set token-uri value))
+  )
+)
+
+;;-------------------------------------
+;; Mint / Burn
+;;-------------------------------------
+
+;; Mint method
+(define-public (mint-for-protocol (amount uint) (recipient principal))
+  (begin
+    (try! (contract-call? .dao check-is-protocol contract-caller))
+    (ft-mint? ststx amount recipient)
+  )
+)
+
+;; Burn method
+(define-public (burn-for-protocol (amount uint) (sender principal))
+  (begin
+    (try! (contract-call? .dao check-is-protocol contract-caller))
+    (ft-burn? ststx amount sender)
+  )
+)
+
+;; Burn external
+(define-public (burn (amount uint))
+  (begin
+    (ft-burn? ststx amount tx-sender)
+  )
+)

example/Clarinet.toml
@@ -4,7 +4,7 @@ telemetry = false
 cache_dir = "./.cache"
 
 [[project.requirements]]
-contract_id = 'SP3FBR2AGK5H9QBDH3EEN6DF8EK8JY7RX8QJ5SVTE.sip-010-trait-ft-standard'
+contract_id = 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.ststx-token'
 
 [contracts.cargo]
 path = "contracts/cargo.clar"

example/contracts/trait.clar
@@ -1,21 +1,3 @@
-(use-trait ft-trait 'SP3FBR2AGK5H9QBDH3EEN6DF8EK8JY7RX8QJ5SVTE.sip-010-trait-ft-standard.sip-010-trait)
-
-(define-public (trait-transfer-function (transfer-data {token: <ft-trait>, amount: uint, recipient: principal}))
-  (let
-    (
-      (ft (get token transfer-data))
-    )
-    (contract-call?
-      ft
-      transfer
-      (get amount transfer-data)
-      tx-sender
-      (get recipient transfer-data)
-      none
-    )
-  )
-)
-
-(define-public (no-trait-function)
+(define-public (function)
   (ok true)
 )

example/contracts/trait.tests.clar
@@ -1,10 +1,9 @@
-(define-public (test-trait
-    (transfer-data {token: <ft-trait>, amount: uint, recipient: principal})
-  )
+(use-trait ft-trait 'SP4SZE494VC2YC5JYG7AYFQ44F5Q4PYV7DVMDPBG.sip-010-trait-ft-standard.sip-010-trait)
+
+(define-public (test-no-trait)
   (ok true)
 )
 
-
-(define-public (test-no-trait)
+(define-public (test-trait (token <ft-trait>))
   (ok true)
 )

commit 08684514423154ec486a077b8b7e9a5dfa0a64a4

Fix `BaseType` bug that slipped in during WIP commit
The bug was introduced in ae0d20c. This commit fixes it.

shared.types.ts
@@ -72,8 +72,8 @@ export type ComplexTypesToCV = {
 
 /** The base Clarity parameter types, as found in the contract interface. */
 export type BaseType =
-  | "int"
-  | "uint"
+  | "int128"
+  | "uint128"
   | "bool"
   | "principal"
   | "trait_reference";