c47b79b8bf056aa5b1a920b6670d2c1cf128bb4f
BowTiedRadone <bowtiedradone@gmail.com>
2025-09-10 16:00:57 +0300

    Rename `sip010.js` to `sip010.cjs` to for Node 18 compatibility

--- example/sip010.cjs
+++ example/sip010.cjs
@@ -13,8 +13,6 @@
 // https://github.com/stacksgov/sips/blob/2c3b36c172c0b71369bc26cffa080e8bdd2b3b84/sips/sip-010/sip-010-fungible-token-standard.md?plain=1#L69
 // https://github.com/stacks-network/sbtc/commit/74daed379e9aad73dd09bebb83231f2c48ef4d81
 
-const { cvToHex } = require("@stacks/transactions");
-
 async function postTransferSip010PrintEvent(context) {
   const selectedFunction = context.selectedFunction;
 
@@ -45,10 +43,6 @@ async function postTransferSip010PrintEvent(context) {
     throw new Error("The memo argument has to be an option type!");
   }
 
-  // Turn the inner value of the `some` type into a hex to compare it with the
-  // print event data.
-  const hexMemoArgumentValue = cvToHex(memoGeneratedArgumentCV.value);
-
   const sip010PrintEvent = functionCallEvents.find(
     (ev) => ev.event === "print_event"
   );
@@ -59,17 +53,16 @@ async function postTransferSip010PrintEvent(context) {
     );
   }
 
-  const sip010PrintEventValue = sip010PrintEvent.data.raw_value;
+  const memoValue = JSON.stringify(memoGeneratedArgumentCV.value);
+  const printEventValue = JSON.stringify(sip010PrintEvent.data.value);
 
-  if (sip010PrintEventValue !== hexMemoArgumentValue) {
+  if (printEventValue !== memoValue) {
     throw new Error(
-      `The print event memo value is not equal to the memo parameter value: ${hexMemoArgumentValue} !== ${sip010PrintEventValue}`
+      `The print event memo value is not equal to the memo parameter value: ${memoValue} !== ${printEventValue}`
     );
   }
 
   return;
 }
 
-module.exports = {
-  postTransferSip010PrintEvent,
-};
+module.exports = { postTransferSip010PrintEvent };
