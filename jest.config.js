f887ca490b6f45ae9e41e87c366c19ab9235d09a
BowTiedRadone <bowtiedradone@gmail.com>
2025-10-01 10:50:59 +0300

    Reduce test parallelization and update `package.json` test scripts
    This commit reduces test parallelization using `maxWorkers` config
    option, moves the testTimeout to jest.config.js, and updates the
    coverage config.

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,22 @@ module.exports = {
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  testTimeout: 15000,
+  maxWorkers: 1,
+  collectCoverage: false,
+  collectCoverageFrom: [
+    // Include: Main TypeScript source files.
+    "*.ts",
+    // Exclude: Test files, config files, and generated content.
+    "!*.tests.ts",
+    "!*.config.ts",
+    "!test.utils.ts",
+    // Exclude: Directories.
+    "!example/**",
+    "!node_modules/**",
+    "!dist/**",
+    "!coverage/**",
+  ],
+  coverageDirectory: "coverage",
 };
